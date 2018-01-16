365a21361d8d1accbc47d97ea7972f1199c803b3
半城天下 <@DESKTOP-D0G123T>
2018-01-16 21:49:31 +0800

    实现基本功能

--- LogDevelop/MainWindow.xaml.cs
+++ LogDevelop/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +25,91 @@ namespace LogDevelop
         public MainWindow()
         {
             InitializeComponent();
+            _Model = new MyModel();
+            this.DataContext = _Model;
         }
+        private MyModel _Model;
 
         private void lstTexts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void onLoadTargetText_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog aDlg = new OpenFileDialog();
+                aDlg.Filter = "文本文件|*.txt;*.htmi;*.log|所有文件|*.*";
+                if (aDlg.ShowDialog() != true) return;
+                _Model.LoadTargetTextForm(aDlg.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void onLoadTargetText_CanExectue(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void onSaveFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                //if (text1.Text != null)
+                {
+
+                    SaveFileDialog saveDlg = new SaveFileDialog();
+                    saveDlg.Filter = "文本文件|*.txt;*.htmi;*.log|所有文件|*.*";
+                    if (true == saveDlg.ShowDialog())
+                    {
+                        _Model.SaveTargetTextForm(saveDlg.FileName);
+                    }
+                }
+               // else
+                {
+                   // MessageBoxResult result = MessageBox.Show("内容为空，无法保存","提示" , MessageBoxButton.OKCancel);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void onSaveFile_CanExectue(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void onCloseFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("退出前请先保存文件, yes or not", "退出", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                SaveFileDialog saveDlg = new SaveFileDialog();
+                saveDlg.Filter = "文本文件|*.txt;*.htmi;*.log|所有文件|*.*";
+                if (true == saveDlg.ShowDialog())
+                {
+                    _Model.SaveTargetTextForm(saveDlg.FileName);
+                }
+            }
+            else if (result == MessageBoxResult.No)
+            {
+                this.Close();
+            }
+        }
+
+        private void onHelp_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("需要什么帮助", "帮助", MessageBoxButton.OKCancel);
+        }
     }
+   
+
+ 
 }
--- LogDevelop/MyModel.cs
+++ LogDevelop/MyModel.cs
@@ -1,32 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace LogDevelop
 {
     class MyModel : INotifyPropertyChanged
     {
-        
+        //正则表达式
+        public string Pattern { get { return _Pattern; } set { if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern)); } }
+        private string _Pattern;
+        //读取的文本内容text
+        public string TargetText { get { return _TargetText; } set { if (_TargetText == value) return; _TargetText = value; OnPropertyChanged(nameof(TargetText)); } }
+        private string _TargetText;
 
-        public void Load()
+        private static Encoding[] _Encodings = new Encoding[]
         {
+            Encoding.Default,
+            Encoding.ASCII,
+            Encoding.Unicode,
+            Encoding.UTF8,
+            Encoding.UTF7,
+            Encoding.UTF32,
+            Encoding.BigEndianUnicode
+        };
 
+        public Encoding[] Encodings { get { return _Encodings; } }
+
+        public Encoding  CurrentEncoding { get { return _CurrentEncoding; } set { if (_CurrentEncoding == value) return; _CurrentEncoding = value; OnPropertyChanged(nameof(CurrentEncoding)); } }
+        private Encoding  _CurrentEncoding=Encoding.Default;
+        
+        //读取文件
+        public void LoadTargetTextForm(string aFileName)
+        {
+            TargetText = File.ReadAllText(aFileName, CurrentEncoding);
         }
+
+
+        
+        //保存文件
+        public void SaveTargetTextForm(string aFileName)
+        {   if(TargetText !=null)
+             File.WriteAllText(aFileName, TargetText, CurrentEncoding);
+            else
+            {
+
+            }
+        }
+
         public void Dofilter()
         {
-
+            
         }
+        
 
-        public string[] SourceText {
-            get {   return _SourceText;   }
-            set {
-                if (_SourceText == value)    return;
-                 _SourceText = value;
-                 OnPropertyChanged(nameof(SourceText)); } }
-        private string[] _SourceText;
 
         private void OnPropertyChanged(string aPropertyName)
         {
@@ -34,5 +65,10 @@ namespace LogDevelop
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void SaveTargetTextForm(string fileName, TextBox textbox1)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
